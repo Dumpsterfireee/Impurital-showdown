2d1f41216821b621ae950e4f8d183f53c3198a89
Dumpsterfireee <cogscape00@gmail.com>
2026-04-17 16:30:13 -0700

    Added Toxicadet & Babblemist

---
+++ data/mods/impuritalfakemon/formats-data.ts
@@ -0,0 +1,8 @@
+export const FormatsData: import('../sim/dex-species').SpeciesFormatsDataTable = {
+	toxicadet: {
+		tier: "UU",
+	},
+	babblemist: {
+		tier: "UU",
+	},
+};
---
+++ data/mods/impuritalfakemon/learnsets.ts
@@ -0,0 +1,32 @@
+export const Learnsets: import('../sim/dex-species').LearnsetDataTable = {
+	toxicadet: {
+		learnset: {
+			poisonfang: ["9L1"],
+			acid: ["9L1"],
+			poisonjab: ["9L1"],
+			sludgebomb: ["9L10"],
+			toxic: ["9L1"],
+			corrosivegas: ["9L13"],
+			lowkick: ["9L16"],
+			axekick: ["9L20"],
+			tackle: ["9L1"],
+			laserfocus: ["9L1"],
+			psychic: ["9L15"],
+			ember: ["9L1"],
+			willowisp: ["9L1"]
+		},
+	},
+		babblemist: {
+		learnset: {
+			jumpkick: ["9L1"],
+			karatechop: ["9L1"],
+			brickbreak: ["9L1"],
+			flashcannon: ["9L10"],
+			irondefense: ["9L1"],
+			ironhead: ["9L13"],
+			thief: ["9L16"],
+			trick: ["9L20"],
+			tackle: ["9L1"]
+		},
+	},
+};
